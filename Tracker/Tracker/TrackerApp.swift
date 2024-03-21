752e6541b680a8abaf2e92d3c96665c7c2118be7
andrewnabaala <andrew.k.nabaala@gmail.com>
2024-03-21 15:47:00 +0300

    Initial Commit

---
+++ Tracker/Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Andrew Nabaala on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
